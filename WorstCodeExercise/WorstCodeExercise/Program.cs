4c2353a10fc08cf7aa21a0105b7b91fed0336ccd
BalRockY <71256208+BalRockY@users.noreply.github.com>
2021-02-13 16:39:26 +0100

    Add files via upload

---
+++ WorstCodeExercise/WorstCodeExercise/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace WorstCodeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+                                                                                     {
+                                                 int one;                  int two;
+
+            Console.
+    WriteLine
+                                                                        ("Choose a number");
+            one 
+                                        =
+      Convert
+                                                        .
+                     ToInt32(
+          Console               .                                       ReadLine()
+          
+          
+          
+          
+          );
+
+            Console.
+                
+                
+                
+                WriteLine                                                                      ("Choose another number");
+            
+            
+            two                         =                       Convert                     
+                
+                
+                
+                .
+                              
+                
+                ToInt32                                 (Console.ReadLine()
+                
+                
+                
+                );
+
+                                                                                                          if 
+            (one != two)
+                                             {
+                Console                                 .
+                    
+                    WriteLine
+                                                    ($"your result: {one} + {two} = " + (one + two));
+                                                                                                 }
+            else
+                                                                                                            {
+                Console.                                WriteLine
+                    
+                    ($"your result: = " + ((one + two) * 3)
+                    
+                    
+                    
+                                                                                                    );
+                                                                                                             }
+
+            
+        }
+    }
+}
